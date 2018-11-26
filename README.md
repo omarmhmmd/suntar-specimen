ba03d84b1e7e01599fc6a659418d66920a2eefe4
omarmhmmd <omar.mhmmd@gmail.com>
2018-11-25 22:02:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# suntar-specimen
---
+++ js/suntar.js
@@ -0,0 +1,138 @@
+new Vue({
+  el: "#container",
+  data: {
+    letters: [{
+        letter: 'a'
+      },
+      {
+        letter: 'b'
+      },
+      {
+        letter: 'c'
+      },
+      {
+        letter: 'd'
+      },
+      {
+        letter: 'e'
+      },
+      {
+        letter: 'f'
+      },
+      {
+        letter: 'g'
+      },
+      {
+        letter: 'h'
+      },
+      {
+        letter: 'i'
+      },
+      {
+        letter: 'j'
+      },
+      {
+        letter: 'k'
+      },
+      {
+        letter: 'l'
+      },
+      {
+        letter: 'm'
+      },
+      {
+        letter: 'n'
+      },
+      {
+        letter: 'o'
+      },
+      {
+        letter: 'p'
+      },
+      {
+        letter: 'q'
+      },
+      {
+        letter: 'r'
+      },
+      {
+        letter: 's'
+      },
+      {
+        letter: 't'
+      },
+      {
+        letter: 'u'
+      },
+      {
+        letter: 'v'
+      },
+      {
+        letter: 'w'
+      },
+      {
+        letter: 'x'
+      },
+      {
+        letter: 'y'
+      },
+      {
+        letter: 'z'
+      },
+      {
+        letter: '.'
+      },
+      {
+        letter: '>'
+      },
+      {
+        letter: '1'
+      },
+      {
+        letter: '2'
+      },
+      {
+        letter: '3'
+      },
+      {
+        letter: '4'
+      },
+      {
+        letter: '5'
+      },
+      {
+        letter: '6'
+      },
+      {
+        letter: '7'
+      },
+      {
+        letter: '8'
+      },
+      {
+        letter: '9'
+      },
+      {
+        letter: '0'
+      },
+      // { letter: '' },
+      // { letter: '' }
+    ],
+    mainLetter: 'a'
+  },
+  methods: {
+    showLetter: function(letter) {
+      console.log(letter);
+      this.mainLetter = letter;
+    }
+  },
+  mounted() {
+
+  },
+  beforeMount() {
+
+  },
+  watch: {
+
+  }
+})
